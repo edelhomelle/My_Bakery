00e1f1ddedb001ea6884c8a08a500aa09ddbc0b6
Eloise <eloise.delhomelle@etu.univ-cotedazur.fr>
2020-05-22 18:02:12 +0200

    debut de code : affichage image

---
+++ boulangerie.js
@@ -0,0 +1,9 @@
+function init() {
+
+
+    var img_chat = document.createElement("img_chat");
+    img.src = "chat.png";
+    var block_chat = document.getElementById("chat");
+    block_chat.appendChild(img_chat);
+
+}
